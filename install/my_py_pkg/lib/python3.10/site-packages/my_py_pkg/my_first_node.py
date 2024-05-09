e97fc1904723093c384fda7faab696671f2cd7b2
Mohammad Abubaker <Mohammad_Abubaker@yahoo.com>
2024-05-09 11:22:00 +0200

    Implemented OOP in my_first.node.py file

--- install/my_py_pkg/lib/python3.10/site-packages/my_py_pkg/my_first_node.py
+++ install/my_py_pkg/lib/python3.10/site-packages/my_py_pkg/my_first_node.py
@@ -2,17 +2,32 @@
 import rclpy
 from rclpy.node import Node
 
+class MyNode(Node):
+    
+    def __init__(self):
+        
+        # Create and initialize custom Node
+        super().__init__("py_test")
+        
+        self.counter_ = 0
+        
+        # Print out message
+        self.get_logger().info("Hello ROS2")
+        
+        self.create_timer(0.5, self.timer_callback)
+        
+    def timer_callback(self):
+        self.counter_ += 1
+        self.get_logger().info("Hello " + str(self.counter_ ))
+
 # Main Function
 def main(args=None):
     
     # Initialize the communication
     rclpy.init(args=args)
     
-    # Create custom Node
-    node = Node("py_test")
-    
-    # Print out message
-    node.get_logger().info("Hello ROS2")
+    # Create a new object
+    node = MyNode()
     
     # 🔥 It is very important, and almost used in all programs
     # Wait / Stay Alive...
